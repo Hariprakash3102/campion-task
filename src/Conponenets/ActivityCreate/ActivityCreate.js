644fc6a5a3c5c93dd2c80f21a23763853d766eaf
Hariprakash3102 <hariprakash.srinivasan@concertidc.com>
2024-07-06 16:18:23 +0530

    second

--- src/Conponenets/ActivityCreate/ActivityCreate.js
+++ src/Conponenets/ActivityCreate/ActivityCreate.js
@@ -1,103 +1,110 @@
 import React, { useState } from 'react';
 import '../Css/ActivityCreate.css';
+import Dropdown from 'react-bootstrap/Dropdown';
+// import Button from 'react-bootstrap/Button';
 import { Icon } from '@iconify/react';
 
 
 const ActivityCreate = () => {
 
+   
   return (
     <div> 
         <div className="row m-1 "  style={{ backgroundColor: '#ffffff' }} >
             <div className='col-12 col-md-6 col-lg-6'>
-                <div className="form-group">
+                <form className="form-group">
                     <label htmlFor="ActivityName " className='mb-3'>Activity Name <span className='text-danger'>*</span></label>
-                    <input type="text" className="form-control mb-3 " id="ActivityName" placeholder="Enter Activity Name" />  
-                </div>  
+                    <input type="text" className="form-control mb-3 p-2" id="ActivityName" placeholder="Enter Activity Name" />  
+                </form>  
             </div>
                 
             <div className="col-12 col-md-6">
-                <div className="form-group">
-                <label htmlFor="ActivityType" className=" mb-3 ">
-                    Activity Type <span className="text-danger">*</span>
-                </label>
-                <select  className="form-control mb-3" id="ActivityType"  >
-                    <option value="disabled" > Select Activity Type   </option>
-                    <option value="Family">Family </option>
-                    <option value="Hunting">Hunting  </option>
-                    <option value="Excursion">Excursion </option>
-                </select>
-                </div>  
+                <form className="form-group">
+                    <label htmlFor="ActivityType" className="mb-3">
+                        Activity Type <span className="text-danger">*</span>
+                    </label>
+                    <Dropdown  className="mb-3">
+                        <Dropdown.Toggle className="form-control bg-white w-100 p-2 border border-none rounded text-start text-dark"  id="ActivityTypeDropdown"  >
+                           {/* <span className='text-start pe-5 '> Enter Activity Type </span> */}
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu className="w-100">
+                            <Dropdown.Item eventKey="Family">Family</Dropdown.Item>
+                            <Dropdown.Item eventKey="Hunting">Hunting</Dropdown.Item>
+                            <Dropdown.Item eventKey="Excursion">Excursion</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown> 
+                </form>
             </div>
 
             <div className='col-12 col-md-6 col-lg-6'>
-                <div className="form-group">
+                <form className="form-group">
                     <label htmlFor="PricingType" className='mb-3' >Pricing Type</label>
-                    <input type="text" className="form-control mb-3 " id="PricingType" placeholder="select Individual/Hour" />  
-                </div>  
+                    <input type="text" className="form-control mb-3  p-2 " id="PricingType" placeholder="select Individual/Hour" />  
+                </form>  
             </div>
 
             <div className='col-12 col-md-6 col-lg-6'>
-                <div className="form-group">
+                <form className="form-group">
                     <label htmlFor="Price" className='mb-3' >Price<span className='text-danger'>*</span></label>
-                    <input type="text" className="form-control mb-3 " id="Price" placeholder="$" />  
-                </div>  
+                    <input type="text" className="form-control mb-3  p-2 " id="Price" placeholder="$" />  
+                </form>  
             </div>
 
             <div className='col-12 col-md-6 col-lg-6'>
-                <div className="form-group">
+                <form className="form-group">
                     <label htmlFor="Image " className='mb-3'>Image</label>
-                    <input type="text " className="form-control p-lg-4 p-xl-5 text-center border border-dotted mb-3" id="PricingType" placeholder="" value={'+ Upload Image'} />  
-                </div>  
+                    <input type="text " className="form-control p-4 p-md-5 text-center border border-dotted mb-3" id="PricingType" placeholder="" value={'+ Upload Image'} />  
+                </form>  
             </div>
 
             <div className='col-12 col-md-6 col-lg-6'>
-                <div className="form-group">
+                <form className="form-group">
                     <label htmlFor="Info " className='mb-3' >Info</label>
-                    <input type="text" className="form-control pt-xl-2 p-lg-4 pb-xl-5 mb-3 " id="Info" placeholder="Enter Info" />  
-                </div>  
+                    <input type="text" className="form-control  p-4 p-md-5  mb-3 " id="Info" placeholder="Enter Info" />  
+                </form>  
             </div> 
 
             <div className='col-12 col-md-6 d-flex flex-row flex wrap'>
-                <div className=' pt-4 pe-3'>
-                <input type="checkbox" className="form-check-input mt-4" id="checklist" />
-                </div>
-                <div className='w-100'>
+                <form className=' pt-4 pe-1'>
+                    <input type="checkbox" className="form-check-input mt-4" id="checklist" />
+                </form>
+                <form className='w-100'>
                     <div className="form-group">
                         <label htmlFor="OverrideCount" className='mb-3' >Override Individual Count<span className='text-danger'>*</span></label>
-                        <input type="text" className="form-control mb-3" id="OverrideCount" placeholder="10" />  
+                        <input type="text" className="form-control mb-3 p-2" id="OverrideCount" placeholder="10" />  
                     </div>  
-                </div>
+                </form>
             </div>
 
             <div className='col-12 col-md-6 col-lg-6'>
-                <div className="form-group">
+                <form className="form-group">
                     <label htmlFor="Price" className='mb-3'>Price<span className='text-danger'>*</span></label>
-                    <input type="text" className="form-control mb-3" id="Price" placeholder="$" />  
-                </div>  
+                    <input type="text" className="form-control mb-3 p-2" id="Price" placeholder="$" />  
+                </form>  
             </div>
 
             <div className='col-12 col-md-6 d-flex flex-row flex-nowrap'>
-                <div className=' pt-4 pe-3'>
-                <input type="checkbox" className="form-check-input mt-4" id="checklist" />
-                </div>
-                <div className='w-100'>
+                <form className=' pt-4 pe-1'>
+                    <input type="checkbox" className="form-check-input mt-4 " id="checklist" />
+                </form>
+                <form className='w-100'>
                     <div className="form-group">
-                        <label htmlFor="OverrideCount" className='mb-3'> Custome Field<span className='text-danger'>*</span></label>
-                        <input type="text" className="form-control mb-3" id="OverrideCount" placeholder="Enter Customer text" />  
+                        <label htmlFor="OverrideCount" className='mb-3 '> Custome Field<span className='text-danger'>*</span></label>
+                        <input type="text" className="form-control mb-3 p-2" id="OverrideCount" placeholder="Enter Customer text" />  
                     </div>  
-                </div>
+                </form>
                 
             </div>
-            <div className='col-12 col-md-6 col-lg-6'>
+            <form className='col-12 col-md-6 col-lg-6'>
                 <div className="form-group">
                     <label htmlFor="Price" className='mb-3'>Price<span className='text-danger'>*</span></label>
-                    <input type="text" className="form-control" id="Price" placeholder="$" />  
+                    <input type="text" className="form-control p-2" id="Price" placeholder="$" />  
                 </div>  
-            </div>
+            </form>
             <div className='text-primary flex-wrap ps-5 pt-2'>  + Add </div> 
             <div className='col d-flex flex-row justify-content-end  mt-5  mb-4 me-5'>
-                <div className='btn border me-3 CancelBtn' style={{ color: '#3f493b', border: '1px solid #3f493b' }}>Cancel</div> 
-                <div className=' btn border SaveBtn text-light'  style={{ backgroundColor: ' #3f493b' }} >Save</div> 
+                <button className=' btn border me-3 CancelBtn bg-light' style={{ color: '#3f493b'}}>Cancel</button> 
+                <button className=' btn border SaveBtn text-light '  style={{ backgroundColor: '#3f493b'}}>Save</button> 
             </div> 
         </div> 
     </div>
--- src/Conponenets/Navbar/Navbar.js
+++ src/Conponenets/Navbar/Navbar.js
@@ -10,6 +10,9 @@ import Store from '../Assets/store.png';
 import Management from '../Assets/Management.png';
 import Report from '../Assets/Report.png';
 import ToggleButton from '../Assets/Toggle.png'
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import Sidebar from '../Sidebar/Sidebar';
 
 const Navbar = () =>{
     const [show, setShow] = useState(false);
@@ -32,56 +35,20 @@ const Navbar = () =>{
                         <p className='m-0 ' style={{fontSize: '12px'}} >super Admin</p>  
                     </div>
                     <div className=" me-3" ><Icon icon="material-symbols:keyboard-arrow-down" /></div>
-                    {/* <div className="d-block d-lg-none bg-light">
-                        <button className="btn" data-bs-toggle="Offcanvas" data-bs-target="#collapseOne" aria-controls="collapseOne" type="button" onClick={handleShow}>
+                    <div  className= " d-block d-lg-none">
+                        <Button variant="white" onClick={handleShow}>
                             <img src={ToggleButton} width={'34px'} height={'34px'} alt="Toggle Button" />
-                        </button>
-                        <div className="Offcanvas Offcanvas-end w-75" id="collapseOne" aria-labelledby="collapseOne" tabIndex="-1"  show={show} onHide={handleClose}>
-                            <div className="Offcanvas-header text-center d-flex justify-content-between">
-                                <div className="p-4 ps-xl-5">
-                                    <a href="#"><img src={logo} width={'100px'} className="" alt="Logo" /></a>
-                                </div>
-                                <button type="button" className="btn-close" data-bs-dismiss="Offcanvas" aria-label="Close"></button>
-                            </div>
-                            <div className="mt-3 Offcanvas-body p-0">
-                                <a href="#" className="col-12 w-auto text-start side_bar text-decoration-none nav-link">
-                                    <img className="me-2 ms-3" src={Dashboard} width={'24px'} height={'24px'} alt="Dashboard Icon" />
-                                    <p className="d-inline-block text-center ps-0 p-3 mb-0">Dashboard</p>
-                                </a>
-                                <div className="col-12 w-auto text-start side_bar">
-                                    <img className="me-2 ms-3" src={Reservation} width={'24px'} height={'24px'} alt="Reservation Icon" />
-                                    <p className="d-inline-block text-center ps-0 p-3 mb-0">Reservation</p>
-                                </div>
-                                <a href="#" className="col-12 w-auto text-start side_bar text-decoration-none nav-link">
-                                    <img className="me-2 ms-3" src={Deer} width={'24px'} height={'24px'} alt="Animal Harvest Icon" />
-                                    <p className="d-inline-block text-center ps-0 p-3 mb-0">Animal Harvest</p>
-                                </a>
-                                <div className="col-12 w-auto text-start side_bar">
-                                    <img className="me-2 ms-3" src={Guest} width={'24px'} height={'24px'} alt="Guest Management Icon" />
-                                    <p className="d-inline-block text-center ps-0 p-3 mb-0">Guest Management</p>
-                                </div>
-                                <div className="col-12 w-auto text-start side_bar">
-                                    <img className="me-2 ms-3" src={Store} width={'24px'} height={'24px'} alt="General Store Icon" />
-                                    <p className="d-inline-block ps-0 p-3 mb-0">General Store</p>
-                                </div>
-                                <div className="col-12 w-auto text-start side_bar">
-                                    <img className="me-2 ms-3" src={Management} width={'24px'} height={'24px'} alt="Management Icon" />
-                                    <p className="d-inline-block text-center ps-0 p-3 mb-0">Management</p>
-                                </div>
-                                <ul className="col-12 w-auto text-start side_bar">
-                                    <li className="d-inline-block text-center ps-0 p-3 mb-0">Room Management</li>
-                                    <li className="d-inline-block text-center ps-0 p-3 mb-0">Activity Management</li>
-                                    <li className="d-inline-block text-center ps-0 p-3 mb-0">Event Management</li>
-                                    <li className="d-inline-block text-center ps-0 p-3 mb-0">Guide Management</li>
-                                    <li className="d-inline-block text-center ps-0 p-3 mb-0">Subscription/Amenities</li>
-                                </ul>
-                                <div className="col-12 w-auto text-start side_bar">
-                                    <img className="me-2 ms-3" src={Report} width={'24px'} height={'24px'} alt="Report Icon" />
-                                    <p className="d-inline-block text-center ps-0 p-3 mb-0">Report</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>  */}
+                        </Button>
+
+                        <Offcanvas show={show} onHide={handleClose}>
+                            <Offcanvas.Header closeButton>
+
+                            </Offcanvas.Header>
+                            <Offcanvas.Body >
+                            <Sidebar />
+                            </Offcanvas.Body>                        
+                        </Offcanvas>
+                    </div>
                 </div> 
                 
             </div>
@@ -90,3 +57,4 @@ const Navbar = () =>{
 }
 
 export default Navbar;
+
